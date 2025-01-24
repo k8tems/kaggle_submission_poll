4cd77b62f3ae4b8d478f65075c2c08a5a56bb633
k8tems <k8tems@live.com>
2025-01-24 22:39:46 +0900

    Continuously poll for new pending submissions and process them accordingly if they finish

--- main.py
+++ main.py
@@ -16,25 +16,24 @@ def get_pending_subs(api):
 if __name__ == '__main__':
     api = KaggleApi()
     api.authenticate()
-    pending = get_pending_subs(api)
-
     # TODO: maybe add all sumissions that aren't in the database in the future?
 
     db = TinyDB('db.json')
 
-    # TODO: まとめて出来ない？
-    for sub in pending:
-        if not db.search(Query().url == sub.url):
-            print(f'adding new entry {sub.url}')
-            db.insert({'url': sub.url, 'status': sub.status, 'date': fmt_dt(sub.date), 'description': sub.description})
-
-    urls = [p.url for p in pending]
-
     while 1:
-        subs = [sub for sub in api.competition_submissions('czii-cryo-et-object-identification') if sub.url in urls]
-        if not subs:
-            break
+        subs = api.competition_submissions('czii-cryo-et-object-identification')
+
         for sub in subs:
+            if sub.status == 'pending' and not db.search(Query().url == sub.url):
+                print(f'adding new entry {sub.url}')
+                db.insert(
+                    {'url': sub.url, 'status': sub.status, 'date': fmt_dt(sub.date), 'description': sub.description})
+
+        for sub in api.competition_submissions('czii-cryo-et-object-identification'):
+            # 上の処理でpendingとしてdbに記録されてる奴のみ処理する
+            if not db.search(Query().url == sub.url):
+                continue
+
             # sub.dateが申請した時間で固定されてる前提の実装
             # 終了後、終了時間に変わるなら練り直しが必要
             elapsed = datetime.utcnow() - sub.date
@@ -43,4 +42,5 @@ if __name__ == '__main__':
                 print(f'submission => {sub.status} sub.date={sub.date} elapsed={elapsed}')
                 db.update({'status': sub.status, 'duration': elapsed.seconds}, Query().url == sub.url)
                 break
+
         time.sleep(60)
