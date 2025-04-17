f1c4f2ae1a3f8d119bfc79f07b78b40f5e583439
k8tems <k8tems@live.com>
2025-04-17 14:17:09 +0900

    [FEATURE] Format date

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from tinydb import TinyDB
 import os
 import glob
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -20,6 +21,14 @@ def format_duration(seconds):
     else:
         return f"00:00:{remaining_seconds:02d}"
 
+def format_date(date_str):
+    # Convert from format YYYY-MM-DD-HH-MM-SS to YYYY/MM/DD HH:MM:SS
+    try:
+        dt = datetime.strptime(date_str, '%Y-%m-%d-%H-%M-%S')
+        return dt.strftime('%Y/%m/%d %H:%M:%S')
+    except:
+        return date_str
+
 @app.route('/')
 def index():
     # Get list of all database files
@@ -35,6 +44,7 @@ def competition(competition_name):
     
     for submission in submissions:
         submission['formatted_duration'] = format_duration(submission.get('duration'))
+        submission['formatted_date'] = format_date(submission['date'])
         # Make URL absolute by adding Kaggle base URL if it's not already absolute
         if submission['url'] and not submission['url'].startswith('http'):
             submission['url'] = f"https://www.kaggle.com{submission['url']}"
