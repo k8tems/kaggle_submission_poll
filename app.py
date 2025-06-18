939e74098966837e7c06e4e52de4851bb3e6aa6e
k8tems_mac <k8tems@live.com>
2025-06-18 14:08:28 +0900

    [FEATURE] Listen on 0.0.0.0:5000

--- app.py
+++ app.py
@@ -106,4 +106,4 @@ def save_cv_score():
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True, host="0.0.0.0", port=5000)
